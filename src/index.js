253372fef16f8d6a58fee6a34ba294a8ad849fc5
Poorvi <pjnayak2003@gmail.com>
2024-03-09 12:00:41 +0530

    Module 3: Setup of React application with Webpack done

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@
 // header.innerText = "Hello from Poorvi";
 // root.appendChild(header);
 
-// THE REACT WAY, WITHOUT JSX
+// THE REACT WAY, WITH AND WITHOUT JSX
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const p1 = React.createElement('p',{},"Hi I'm child 1");
 const p2 = React.createElement('p',{},"Hi I'm child 2");
-const div = React.createElement('div',{className: 'colorChange'},[p1,p2]);
+// const div = React.createElement('div',{className: 'colorChange'},[p1,p2]);
+const div = <div className='colorChange'>Hello</div>
 console.log(div);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(div);
