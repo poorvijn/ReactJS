7584bb8bb0adab70e5066bca66c5b2ad10bd8bc1
Poorvi <pjnayak2003@gmail.com>
2024-03-09 10:45:31 +0530

    Completed module 2

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const root = document.getElementById('root');
-const header = document.createElement('h1');
-header.innerText = "Hello from Poorvi";
-root.appendChild(header);
+//  THE VANILLA JAVASCRIPT WAY
+// const root = document.getElementById('root');
+// const header = document.createElement('h1');
+// header.innerText = "Hello from Poorvi";
+// root.appendChild(header);
+
+// THE REACT WAY, WITHOUT JSX
+const p1 = React.createElement('p',{},"Hi I'm child 1");
+const p2 = React.createElement('p',{},"Hi I'm child 2");
+const div = React.createElement('div',{className: 'colorChange'},[p1,p2]);
+console.log(div);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(div);
