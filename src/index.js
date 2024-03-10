2543f648e3b8e2665a7f76214475a14611da2492
Poorvi <pjnayak2003@gmail.com>
2024-03-10 11:31:38 +0530

    Can write normal HTML code in JSX

--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import ReactDOM from 'react-dom';
 const p1 = React.createElement('p',{},"Hi I'm child 1");
 const p2 = React.createElement('p',{},"Hi I'm child 2");
 // const div = React.createElement('div',{className: 'colorChange'},[p1,p2]);
-const div = <div className='colorChange'>Hello</div>
+const div = <div className='colorChange'>
+    <p>Hi</p>
+</div>
 console.log(div);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(div);
